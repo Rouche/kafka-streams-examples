0a67f803ee8b318fbfa909382972455c6405ff41
jfLAROUCHE.externe <jfLAROUCHE.externe@macif.fr>
2020-01-06 13:30:14 -0500

    Kafka streams

--- src/main/java/io/confluent/examples/streams/ReadUsers.java
+++ src/main/java/io/confluent/examples/streams/ReadUsers.java
@@ -15,6 +15,11 @@
  */
 package io.confluent.examples.streams;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Properties;
 
 import org.apache.kafka.common.serialization.Serde;
@@ -25,6 +30,12 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
  * Demonstrates group-by operations and aggregations on KTable. In this specific example we
@@ -95,58 +106,67 @@ import org.apache.kafka.streams.kstream.Produced;
  * 6) Once you're done with your experiments, you can stop this example via {@code Ctrl-C}. If needed,
  * also stop the Kafka broker ({@code Ctrl-C}), and only then stop the ZooKeeper instance ({@code Ctrl-C}).
  */
-public class LargeRegionEnumerate {
+public class ReadUsers {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws Exception {
         final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
         final Properties streamsConfiguration = new Properties();
         // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
         // against which the application is run.
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-region-enumerate-example");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "user-region-enumerate-example-client");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "read-user-example");
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "read-user-client");
         // Where to find Kafka broker(s).
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         // Specify default (de)serializers for record keys and for record values.
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
         // Records should be flushed every 10 seconds. This is less than the default
         // in order to keep this example interactive.
         streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
 
-        final Serde<String> stringSerde = Serdes.String();
-        final Serde<Long> longSerde = Serdes.Long();
+//        final StreamsBuilder builder = new StreamsBuilder();
+//
+//        final KTable<String, Long> users = builder.table("USERS");
+//
+//        KafkaStreams stream = new KafkaStreams(builder.build(), streamsConfiguration);
+//
+//        stream.cleanUp();
+//        stream.start();
+//
+//        final String queryableStoreName = users.queryableStoreName();
+//
+//        ReadOnlyKeyValueStore view = stream.store(queryableStoreName, QueryableStoreTypes.keyValueStore());
+//
+//        System.out.println(view.get("User_5").toString());
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(stream::close));
 
-        final StreamsBuilder builder = new StreamsBuilder();
+        final HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
 
-        final KTable<String, Long> userRegions = builder.table("LargeRegions");
+        final String body = "{\n" +
+                "  \"ksql\": \"select * from users limit 20;\",\n" +
+                "  \"streamsProperties\": {\n" +
+//                "    \"ksql.streams.auto.offset.reset\": \"latest\"\n" +
+                "  }\n" +
+                "}";
 
-        final KStream<String, Long> regionsStream = userRegions
-                .toStream();
+        final HttpRequest request = HttpRequest.newBuilder()
+                .header("Accept", "application/vnd.ksql.v1+json")
+                .header("Content-Type", "application/vnd.ksql.v1+json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create("http://localhost:8088/query"))
+                .build();
 
-        regionsStream.foreach((s, aLong) -> {
-            System.out.format("Key [%s] Value: [%d]\n", s, aLong);
-        });
+        final HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // write to the result topic, we need to override the value serializer to for type long
-        regionsStream.to("RegionsEnum", Produced.with(stringSerde, longSerde));
 
-        final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
-        // Always (and unconditionally) clean local state prior to starting the processing topology.
-        // We opt for this unconditional call here because this will make it easier for you to play around with the example
-        // when resetting the application for doing a re-run (via the Application Reset Tool,
-        // http://docs.confluent.io/current/streams/developer-guide.html#application-reset-tool).
-        //
-        // The drawback of cleaning up local state prior is that your app must rebuilt its local state from scratch, which
-        // will take time and will require reading all the state-relevant data from the Kafka cluster over the network.
-        // Thus in a production scenario you typically do not want to clean up always as we do here but rather only when it
-        // is truly needed, i.e., only under certain conditions (e.g., the presence of a command line flag for your app).
-        // See `ApplicationResetExample.java` for a production-like example.
-        streams.cleanUp();
-        streams.start();
+        // print status code
+        System.out.println(response.statusCode());
 
-        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
-
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        // print response body
+        System.out.println(response.body());
     }
 
 }
