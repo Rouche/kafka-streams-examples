54ebe703c743a14c28c9c3b9a07db5c5e3ced6ef
jfLAROUCHE.externe <jfLAROUCHE.externe@macif.fr>
2020-01-15 15:39:50 -0500

    Kafka 5.4.0

--- src/main/java/com/example/demo/DemoKafkaStreamConfig.java
+++ src/main/java/com/example/demo/DemoKafkaStreamConfig.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.state.KeyValueStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,84 +24,82 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Configuration
 public class DemoKafkaStreamConfig {
-	private static final Logger logger = LoggerFactory.getLogger(DemoKafkaStreamConfig.class);
 
-	@Bean
-	public KafkaStreams kafkaStreams(KafkaProperties kafkaProperties,
-	                                 Topology kafkaStreamTopology,
-	                                 @Value("${spring.application.inputTopic}") String inputTopic,
-	                                 @Value("${spring.application.name}") String appName) throws ExecutionException, InterruptedException {
-		final Properties props = new Properties();
+    private static final Logger logger = LoggerFactory.getLogger(DemoKafkaStreamConfig.class);
 
-		// inject SSL related properties
-		props.putAll(kafkaProperties.getSsl().buildProperties());
-		props.putAll(kafkaProperties.buildStreamsProperties());
+    @Bean
+    public KafkaStreams kafkaStreams(KafkaProperties kafkaProperties,
+                                     Topology kafkaStreamTopology,
+                                     @Value("${spring.application.inputTopic}") String inputTopic,
+                                     @Value("${spring.application.name}") String appName) throws ExecutionException, InterruptedException {
+        final Properties props = new Properties();
 
-		// stream config centric ones
-		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, appName);
-		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
-		props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, JsonNode.class);
+        // inject SSL related properties
+        props.putAll(kafkaProperties.getSsl().buildProperties());
+        props.putAll(kafkaProperties.buildStreamsProperties());
 
-		//Initialiser la topic
-		initTopic(inputTopic, props);
+        // stream config centric ones
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, appName);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, JsonNode.class);
 
-		//Connect streams
-		final KafkaStreams kafkaStreams = new KafkaStreams(kafkaStreamTopology, props);
-		kafkaStreams.start();
+        //Initialiser la topic
+        initTopic(inputTopic, props);
 
-		return kafkaStreams;
-	}
+        //Connect streams
+        final KafkaStreams kafkaStreams = new KafkaStreams(kafkaStreamTopology, props);
+        kafkaStreams.start();
 
-	/**
-	 * Créer la topic si nécessaire
-	 * @param inputTopic nom de la Topic
-	 * @param props propriétés kafka
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
-	private void initTopic(String inputTopic, Properties props) throws InterruptedException, ExecutionException {
-		AdminClient kafkaAdminClient = KafkaAdminClient.create(props);
-		DescribeTopicsResult describeTopicsResult = kafkaAdminClient.describeTopics(Collections.singletonList(inputTopic));
-		try {
-			describeTopicsResult.all().get();
-			logger.info("Found topic {}", inputTopic);
-		} catch (ExecutionException e) {
-			logger.warn("Topic not found. Creating {}", inputTopic);
-			NewTopic topic = getNewKTableTopic(inputTopic);
-			CreateTopicsResult result = kafkaAdminClient.createTopics(Collections.singletonList(topic));
-			result.all().get();
-		}
-	}
+        return kafkaStreams;
+    }
 
-	private NewTopic getNewKTableTopic(String name) {
-		NewTopic topic = new NewTopic(name, 1, (short) 1);
-		Map<String, String> configs = new HashMap<>();
-		configs.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT);
-		configs.put(TopicConfig.RETENTION_MS_CONFIG, "10000");
-		configs.put(TopicConfig.MAX_COMPACTION_LAG_MS_CONFIG, "1000");
-		topic.configs(configs);
-		return topic;
-	}
+    /**
+     * Créer la topic si nécessaire
+     *
+     * @param inputTopic nom de la Topic
+     * @param props      propriétés kafka
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    private void initTopic(String inputTopic, Properties props) throws InterruptedException, ExecutionException {
+        AdminClient kafkaAdminClient = KafkaAdminClient.create(props);
+        DescribeTopicsResult describeTopicsResult = kafkaAdminClient.describeTopics(Collections.singletonList(inputTopic));
+        try {
+            describeTopicsResult.all().get();
+            logger.info("Found topic {}", inputTopic);
+        } catch (ExecutionException e) {
+            logger.warn("Topic not found. Creating {}", inputTopic);
+            NewTopic topic = getNewKTableTopic(inputTopic);
+            CreateTopicsResult result = kafkaAdminClient.createTopics(Collections.singletonList(topic));
+            result.all().get();
+        }
+    }
 
-	@Bean
-	public Topology kafkaStreamTopology(@Value("${spring.application.inputTopic}") String inputTopic,
-	                                    @Value("${spring.application.stateStoreName}") String stateStoreName) {
-		final StreamsBuilder streamsBuilder = new StreamsBuilder();
+    private NewTopic getNewKTableTopic(String name) {
+        NewTopic topic = new NewTopic(name, 1, (short) 1);
+        Map<String, String> configs = new HashMap<>();
+        configs.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT);
+        configs.put(TopicConfig.RETENTION_MS_CONFIG, "10000");
+        configs.put(TopicConfig.MAX_COMPACTION_LAG_MS_CONFIG, "1000");
+        topic.configs(configs);
+        return topic;
+    }
 
-		streamsBuilder.table(inputTopic,
-				Consumed.with(Serdes.String(), Serdes.String()),
-				Materialized.as(stateStoreName));
+    @Bean
+    public Topology kafkaStreamTopology(@Value("${spring.application.inputTopic}") String inputTopic,
+                                        @Value("${spring.application.stateStoreName}") String stateStoreName) {
+        final StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-		return streamsBuilder.build();
-	}
+        streamsBuilder.table(inputTopic,
+                Consumed.with(Serdes.String(), Serdes.String()),
+                Materialized.as(stateStoreName));
+
+        return streamsBuilder.build();
+    }
 }
